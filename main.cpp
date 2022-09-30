bba109c607a1d96a4429f24f2636db8f54da0a6e
Serdar Buğra Kala <96396046+SBKala@users.noreply.github.com>
2022-09-30 12:23:36 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int CheckSumPossibility(int num, int arr[], int size)
+{
+if (num == 0)
+    return true;
+if (size == 0)
+    return false;
+
+if (arr[size - 1] > num)
+    return CheckSumPossibility(num, arr, size - 1);
+
+return CheckSumPossibility(num, arr, size - 1) ||
+    CheckSumPossibility(num - arr[size - 1], arr, size - 1);
+	return 0;
+	}
+
+int main(int argc, char** argv) {
+	int MAX_SIZE = 8;
+	int arr[MAX_SIZE];
+	int arraySize = sizeof(arr) / sizeof(arr[0]);
+    int num = 129;
+	int returnVal;
+	int i; 
+      
+	for(i = 0; i < arraySize; ++i)
+	{
+		cin >> arr[i];
+	}
+
+returnVal = CheckSumPossibility(num, arr, arraySize);
+	
+	if(returnVal ==1)
+	{
+		cout << "Possible!" << endl;
+	}
+	else
+	{
+		cout << "Not possible!" << endl;
+	}
+	return 0;
+}
